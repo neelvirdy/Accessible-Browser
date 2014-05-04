4729223c3b4071736ae7174324a982c023338d12
Neel Virdy <neelvirdy@gmail.com>
2014-05-04 17:15:45 -0400

    Updated sidebar buttons html

--- content.js
+++ content.js
@@ -12,7 +12,10 @@ var sidebar_html = ['<div class="symba" id="symba-navigation">',
 		'<li class="symba"><input type="image" src="' + extension_url + 'images/back.png" alt="Back" id="symba-back-button" class="symba" height="32" width="32"></li>',
 		'<li class="symba"><input type="image" src="' + extension_url + 'images/forward.png" alt="Forward" id="symba-forward-button" class="symba" height="32" width="32"></li>',
 		'<li class="symba"><input type="image" src="' + extension_url + 'images/new-tab.png" alt="New Tab" id="symba-new-tab-button" class="symba" height="32" width="32"></li>',
+		'<li class="symba"><input type="image" src="' + extension_url + 'images/switch-tab.png" alt="Switch Tab" id="symba-switch-tab-button" class="symba" height="32" width="32"></li>',
+		'<li class="symba"><input type="image" src="' + extension_url + 'images/refresh.png" alt="Refresh" id="symba-refresh-button" class="symba" height="32" width="32"></li>',
 		'<li class="symba"><input type="image" src="' + extension_url + 'images/pause.png" alt="Pause" id="symba-pause-button" class="symba" height="32" width="32"></li>',
+		'<li class="symba"><input type="image" src="' + extension_url + 'images/leave.png" alt="Leave" id="symba-leave-button" class="symba" height="32" width="32"></li>',
 	'</ul>',
 '</div>'].join('\n');
 
@@ -58,6 +61,17 @@ $('#symba-forward-button').click(function(event){
 
 $('#symba-new-tab-button').click(function(event){
 	window.open("https://www.google.com", '_newtab');
+});
+
+$('#symba-switch-tab-button').click(function(event){
+	e = jQuery.Event("keydown");        
+	e.which = 9;
+	e.ctrlKey = true;
+	$("html").trigger(e);
+});
+
+$('#symba-refresh-button').click(function(event){
+	window.refresh();
 });
 
 $('#symba-pause-button').click(function(event){
@@ -134,7 +148,7 @@ function cycleContent(){
 
 		if(!$(options[index]).hasClass('symba')){
 		
-			if(index == 0) {
+			if(index == 0 && options.length > 0) {
 				$('body').animate({			
         			scrollTop: $(parent).position().top
     			}, SCROLL_SPEED * 0.8);
